82213d4d3d7fd77b53313b6513f49fcd0069351e
Georgiana <gitan_georgiana@yahoo.com>
2018-07-11 20:07:45 +0300

    Sectiunea de administrare

---
+++ project/admin/login-form.php
@@ -0,0 +1,35 @@
+<form method="POST">
+	<input type="hidden" name="comanda" value="login">
+	<table cellpadding="0" cellspacing="2" align="left" width="168">
+		<tr>
+			<td colspan="2"><b>Autentificare:</b></td>
+		</tr>
+		<tr>
+			<td width="70">Nume:</td>
+			<td><input type="text" name="nume" size="30"></td>
+		</tr>
+		<tr>
+			<td width="70">Parola:</td>
+			<td><input type="password" name="parola" size="30" value=""></td>
+		</tr>
+		<tr>
+			<td colspan="2" align="center"><input type="submit" value="Login"></td>
+		</tr>
+	</table>	
+</form>
+
+<!-- <form method="POST">
+
+	<div class="input-group">
+		<label>Username</label>
+		<input type="text" name="nume" >
+	</div>
+	<div class="input-group">
+		<label>Password</label>
+		<input type="password" name="parola">
+	</div>
+	<div class="input-group">
+		<button type="submit" value="Login">Login</button>
+	</div>
+	
+</form> -->
