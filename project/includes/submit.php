d00a6546f88a5832a4c5f6cc8b0b39f9897ae885
Georgiana <gitan_georgiana@yahoo.com>
2018-07-12 23:31:20 +0300

    Adaugare update pentru cursuri

--- project/includes/submit.php
+++ project/includes/submit.php
@@ -29,28 +29,35 @@ if(isset($_POST['submit'])){
             // Face update daca a gasit persoana
             $sql = "UPDATE persoana SET email='$email', telefon='$telefon', ocupatie='$ocupatie' WHERE nume = '".$nume."' AND prenume = '".$prenume."'";
             
-            // Vectorul legaturilor (id_p,id_c)
-            $pc = "SELECT * FROM persoana_curs";
-            $rez = mysqli_query($conn, $pc);
-            $pcarray = array();
+            // Vectorul ce contine legaturile (id_p,id_c)
+            $pers_curs = "SELECT * FROM persoana_curs";
+            $rez = mysqli_query($conn, $pers_curs);
+            $pers_curs_array = array();
             if(mysqli_num_rows($rez) > 0){
                 while ($row = mysqli_fetch_assoc($rez)){
-                 $pcarray[] = $row;
+                 $pers_curs_array[] = $row;
                 }
-            }   
-            foreach ($pcarray as $pcarr) {
-                if($data['id_p'] == $pcarr['id_p']){
-                    foreach($cursuri as $curs){
-                        if($curs != $pcarr['id_c']){
-                            //insereaza 
-                        }
-                    }
-
+            } 
+            // Vector ce va retine cursurile noi ce se vor concatena la cele existente 
+            $curs_de_introdus = array();
+            foreach ($pers_curs_array as $pers_curs_arr) {
+                // Daca se gaseste id-ul persoanei in tabelul persoana_curs
+                if($data['id_p'] == $pers_curs_arr['id_p']){
+                    // Se creaza un sir ce retine cursul asociat persoanei
+                    $pers_curs_id_c[] = $pers_curs_arr['id_c'];
+                    // Daca se gaseste in BD cursul ales se sterge din cursurile ce urmeaza sa fie concatenate
+                    $curs_de_introdus = array_diff($cursuri, $pers_curs_id_c);
+                    $cursuri = $curs_de_introdus;
                 }
             }
-            
+            // print_r(array_values($cursuri));
+
+            foreach($cursuri as $curs){
+                $sql2 = "INSERT INTO persoana_curs (id_p, id_c) VALUES ('".$data['id_p']."', '$curs') ";
+                mysqli_query($conn, $sql2);
+            }
+
             $find = true;
-            
             mysqli_query($conn, $sql);  
         }
     }
