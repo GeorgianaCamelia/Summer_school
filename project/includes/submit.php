f488d2ac159ec334a9629c7b9a71cca038b455b9
Georgiana <gitan_georgiana@yahoo.com>
2018-07-12 15:38:18 +0300

    Adaugare cod pentru insert/update in tabelul persoana si insert persoana_curs

--- project/includes/submit.php
+++ project/includes/submit.php
@@ -14,17 +14,69 @@ if(isset($_POST['submit'])){
     $telefon            = mysqli_real_escape_string($conn, $_POST['telefon']);
     $cursuri            = $_POST['cursuri']; // de ce ? :'()
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~TESTE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     $sql = "INSERT INTO persoana(nume, prenume, email) VALUES ('$nume' , '$prenume', '$email')";
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~UPDATE/INSERT~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Vector cu persoanele din BD
+    $query = "SELECT * FROM persoana";
+    $result = mysqli_query($conn, $query);
+    $datas = array();
+    if(mysqli_num_rows($result) > 0){
+        while ($row = mysqli_fetch_assoc($result)){
+            $datas[] = $row;
+        }
+    }
+    // Cauta dupa nume si prenume o pers in BD
+    $find = false;
+    foreach($datas as $data){
+        if ($data['nume'] == $nume && $data['prenume'] == $prenume){
+            // Face update daca a gasit persoana
+            $sql = "UPDATE persoana SET email='$email', telefon='$telefon', ocupatie='$ocupatie' WHERE nume = '".$nume."' AND prenume = '".$prenume."'";
+            
+            // Vectorul legaturilor (id_p,id_c)
+            $legatura = "SELECT * FROM persoana_curs";
+            $rez = mysqli_query($conn, $legatura);
+            $legarray = array();
+            if(mysqli_num_rows($rez) > 0){
+                while ($row = mysqli_fetch_assoc($rez)){
+                 $legarray[] = $row;
+                }
+            }   
+            
+            $find = true;
+        }
+    }
 
-     echo $nume;
-     echo $prenume;
-     echo $email;
-     echo $ocupatie;
-     echo $telefon;
-     print_r($cursuri);
+    // Daca nu a gasit persoana in BD
+    if($find == false){
+        // Insereaza in tabelul persoana
+        $sql = "INSERT INTO persoana(nume, prenume, email, telefon, ocupatie) VALUES ('$nume' , '$prenume', '$email', '$telefon', '$ocupatie')";
 
-     mysqli_query($conn, $sql);
+        // Cauta ultimul id
+        if ($conn->query($sql) === TRUE) {
+                $last_id = $conn->insert_id;
+                echo "New record created successfully. Last inserted ID is: " . $last_id;
+                
+                // Introduce id-ul ultimei persoane inserate si id-urile cursurilor alese
+                foreach($cursuri as $curs){
+                    $sql2 = "INSERT INTO persoana_curs (id_p, id_c) VALUES ('$last_id', '$curs') ";
+                    mysqli_query($conn, $sql2);
+                }
+        
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+    }
+
+    
+    
+    echo $nume." ";
+    echo $prenume." ";
+    echo $email." ";
+    echo $ocupatie." "; 
+    echo $telefon." ";
+    print_r($cursuri);
+    
+    mysqli_query($conn, $sql);
+  
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 }
 ?>
